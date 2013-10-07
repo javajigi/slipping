16cbce18825d85feb388f1211f2176144caa1775
javajigi <javajigi@gmail.com>
2013-10-07 13:09:42 +0900

    MySQL 데이터베이스에서 적용 가능하도록 수정함.

---
+++ slipp-qna/src/main/java/net/slipp/repository/qna/QuestionRepository.java
@@ -0,0 +1,9 @@
+package net.slipp.repository.qna;
+
+import net.slipp.domain.qna.Question;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface QuestionRepository extends CrudRepository<Question, Long> {
+
+}
