334edfda942791994b2cd56554374770d4c7d4f7
javajigi <javajigi@gmail.com>
2013-10-12 14:30:28 +0900

    데이터베이스 생성 예제 추가함.

--- slipp-qna/src/test/java/net/slipp/support/ApplicationContextTest.java
+++ slipp-qna/src/test/java/net/slipp/support/ApplicationContextTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:infrastructure.xml")
+@ContextConfiguration("classpath:applicationContext.xml")
 public class ApplicationContextTest {
 	@Autowired
 	private DataSource dataSource;
